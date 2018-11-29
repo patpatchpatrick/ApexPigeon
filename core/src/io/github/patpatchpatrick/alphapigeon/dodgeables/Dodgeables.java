1f3e971e61ff145805618b0bb7eced0ce2fef8b2
Patrick Doyle <patrickdoyle30@gmail.com>
2018-11-28 20:02:39 -0800

    Finish energy beam complete animation

--- core/src/io/github/patpatchpatrick/alphapigeon/dodgeables/Dodgeables.java
+++ core/src/io/github/patpatchpatrick/alphapigeon/dodgeables/Dodgeables.java
@@ -105,15 +105,26 @@ public class Dodgeables {
     private Array<Body> energyBallArray = new Array<Body>();
     private Animation<TextureRegion> energyBallAnimation;
     private Texture energyBallSheet;
-    private float energyBallWidth = 80f;
-    private float energyBallHeight = 40f;
+    private float energyBallWidth = 5f;
+    private float energyBallHeight = energyBallWidth/2;
+    private float energyBallFrameNumber = 0;
+    private boolean energyBallSizeEqualsBeamSize = false;
+    private boolean energyBeamAnimationComplete = false;
 
     //UFO Energy Beam variables
     private Array<Body> energyBeamArray = new Array<Body>();
     private Animation<TextureRegion> energyBeamAnimation;
     private Texture energyBeamSheet;
-    private float energyBeamWidth = 80f;
-    private float energyBeamHeight = 40f;
+    private final float ENERGY_BEAM_WIDTH = 80f;
+    private final float ENERGY_BEAM_HEIGHT = 40f;
+
+    //UFO Static Energy Beam variables
+    private Animation<TextureRegion> energyBeamStaticAnimation;
+    private Texture energyBeamStaticSheet;
+    private float ENERGY_BEAM_STATIC_WIDTH = 80f;
+    private float ENERGY_BEAM_STATIC_HEIGHT = 40f;
+
+
 
     //PowerUp Shield variables
     private Array<Body> powerUpShieldsArray = new Array<Body>();
@@ -149,6 +160,7 @@ public class Dodgeables {
         initializeAlienMissileCornerAnimation();
         initializeEnergyBallAnimation();
         initializeEnergyBeamAnimation();
+        initializeEnergyBeamStaticAnimation();
 
         // initialize powerup animations
         initializePowerUpShieldAnimation();
@@ -606,12 +618,22 @@ public class Dodgeables {
         TextureRegion alienMissileExplosionCurrentFrame = alienMissileExplosionAnimation.getKeyFrame(stateTime, true);
         TextureRegion alienCornerCurrentFrame = alienMissileCornerAnimation.getKeyFrame(stateTime, true);
         TextureRegion energyBallCurrentFrame = energyBallAnimation.getKeyFrame(stateTime, true);
+        TextureRegion energyBeamStaticCurrentFrame = energyBeamStaticAnimation.getKeyFrame(stateTime, true);
         TextureRegion energyBeamCurrentFrame = energyBeamAnimation.getKeyFrame(stateTime, true);
         TextureRegion powerUpShieldCurrentFrame = powerUpShieldAnimation.getKeyFrame(stateTime, true);
         TextureRegion teleportCurrentFrame = teleportAnimation.getKeyFrame(stateTime, true);
 
-        //batch.draw(energyBallCurrentFrame, 0, 5, 0, 0, energyBallWidth, energyBallHeight, 1, 1, 0);
-        batch.draw(energyBeamCurrentFrame, 0, 5, 0, 0, energyBeamWidth, energyBeamHeight, 1, 1, 0);
+        if (!energyBallSizeEqualsBeamSize){
+            batch.draw(energyBallCurrentFrame, -2 + ENERGY_BEAM_WIDTH - energyBallWidth, 5 + ENERGY_BEAM_HEIGHT/2 - energyBallHeight/2, 0, 0, energyBallWidth, energyBallHeight, 1, 1, 0);
+        } else if (!energyBeamAnimationComplete) {
+            batch.draw(energyBeamCurrentFrame, -2, 5, 0, 0, ENERGY_BEAM_WIDTH, ENERGY_BEAM_HEIGHT, 1, 1, 0);
+        } else {
+            batch.draw(energyBallCurrentFrame, -2 , 5, 0, 0, ENERGY_BEAM_WIDTH, ENERGY_BEAM_HEIGHT, 1, 1, 0);
+            batch.draw(energyBeamStaticCurrentFrame, -2, 5, 0, 0, ENERGY_BEAM_STATIC_WIDTH, ENERGY_BEAM_STATIC_HEIGHT, 1, 1,0);
+            batch.draw(energyBeamCurrentFrame, -2, 5, 0, 0, ENERGY_BEAM_WIDTH, ENERGY_BEAM_HEIGHT, 1, 1, 0);
+
+        }
+
 
         // draw all level one birds dodgeables using the current animation frame
         for (Body backwardsPigeon : levelOneBirdsArray) {
@@ -725,6 +747,17 @@ public class Dodgeables {
     public void update(float stateTime) {
         //Check if we need to spawn new dodgeables depending on game time
         spawnDodgeables();
+
+        energyBallWidth = energyBallWidth + 0.2f;
+        energyBallHeight = energyBallWidth /2;
+        if (energyBallWidth >= ENERGY_BEAM_WIDTH){
+            energyBallSizeEqualsBeamSize = true;
+            energyBallFrameNumber++;
+            if (energyBallFrameNumber > 12){
+                energyBeamAnimationComplete = true;
+            }
+        }
+
 
         // ROCKETS
         // If rockets are spawned , accelerate them.  The X force is constant and the Y force
@@ -1116,14 +1149,14 @@ public class Dodgeables {
     private void initializeEnergyBeamAnimation() {
 
         // Load the energy beam sprite sheet as a Texture
-        energyBallSheet = new Texture(Gdx.files.internal("sprites/EnergyBeamSpriteSheet.png"));
+        energyBeamSheet = new Texture(Gdx.files.internal("sprites/EnergyBeamSpriteSheet.png"));
 
         // Use the split utility method to create a 2D array of TextureRegions. This is
         // possible because this sprite sheet contains frames of equal size and they are
         // all aligned.
-        TextureRegion[][] tmp = TextureRegion.split(energyBallSheet,
-                energyBallSheet.getWidth() / 3,
-                energyBallSheet.getHeight() / 4);
+        TextureRegion[][] tmp = TextureRegion.split(energyBeamSheet,
+                energyBeamSheet.getWidth() / 3,
+                energyBeamSheet.getHeight() / 4);
 
         // Place the regions into a 1D array in the correct order, starting from the top
         // left, going across first. The Animation constructor requires a 1D array.
@@ -1136,7 +1169,37 @@ public class Dodgeables {
         }
 
         // Initialize the Animation with the frame interval and array of frames
-        energyBeamAnimation = new Animation<TextureRegion>(0.05f, frames);
+        energyBeamAnimation = new Animation<TextureRegion>(0.07f, frames);
+
+
+    }
+
+    private void initializeEnergyBeamStaticAnimation() {
+
+        // Load the energy beam static sprite sheet as a Texture
+        // The static energy beam is the energy beam animation after the energy beam has been shot
+        // out of the laser and remains in a straight line
+        energyBeamStaticSheet = new Texture(Gdx.files.internal("sprites/EnergyBeamStaticSpriteSheet.png"));
+
+        // Use the split utility method to create a 2D array of TextureRegions. This is
+        // possible because this sprite sheet contains frames of equal size and they are
+        // all aligned.
+        TextureRegion[][] tmp = TextureRegion.split(energyBeamStaticSheet,
+                energyBeamStaticSheet.getWidth() / 3,
+                energyBeamStaticSheet.getHeight() / 3);
+
+        // Place the regions into a 1D array in the correct order, starting from the top
+        // left, going across first. The Animation constructor requires a 1D array.
+        TextureRegion[] frames = new TextureRegion[3 * 3];
+        int index = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                frames[index++] = tmp[i][j];
+            }
+        }
+
+        // Initialize the Animation with the frame interval and array of frames
+        energyBeamStaticAnimation = new Animation<TextureRegion>(0.05f, frames);
 
 
     }
@@ -1210,6 +1273,9 @@ public class Dodgeables {
         alienMissileCornerExplosionSheet.dispose();
         rocketExplosionSheet.dispose();
         rocketSheet.dispose();
+        energyBeamSheet.dispose();
+        energyBallSheet.dispose();
+        energyBeamStaticSheet.dispose();
     }
 
 }
